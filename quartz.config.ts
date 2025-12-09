60f0103320a598966792e024345622edaf93f016
locheroldman-afk <locheroldman@gmail.com>
2025-12-09 16:37:47 +0800

    Quartz sync: Dec 9, 2025, 4:37 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Caveat",
+        body: "Noto Sans",
         code: "IBM Plex Mono",
       },
       colors: {
